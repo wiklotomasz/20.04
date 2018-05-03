dc74b1c84e5def22cc3f4590d1f4a5ff4bb5a4a1
Tomasz Wiklo <wikweb@gmail.com>
2018-05-03 22:25:37 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import { render } from 'react-dom';
 import {Router, hashHistory} from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
+import DevTools from "./DevTools";
 import routes from './routes';
 
 
 render(
-	<Provider store={store}>
-		<Router history={hashHistory} routes={routes}/>
-		<div>
-			<DevTools />
-			<h1>Inicjacja projektu</h1>
-		</div>
-	</Provider>,	
-	document.getElementById('root')
-	);
+  <Provider store={store}>
+    <div>
+      <Router history={hashHistory} routes={routes} />
+      <DevTools />
+    </div>
+  </Provider>,
+  document.getElementById("root")
+);
